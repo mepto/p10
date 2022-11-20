7040d4e2757c1bd9054579480013ff0023bbe7ca
mepto <natg.inf@gmail.com>
2022-11-20 22:50:26 +0100

    Change modified date for issue on update

--- softdesk/views/issue.py
+++ softdesk/views/issue.py
@@ -28,3 +28,20 @@ class IssueViewSet(ModelViewSet):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        """Change modified data to issue on update."""
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.initial_data['modified'] = now()
+        # TODO update modified by field after authentication
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
