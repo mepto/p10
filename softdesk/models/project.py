9cefa9b11344879585bc5efa40da2c0536baada6
mepto <natg.inf@gmail.com>
2022-12-26 13:32:51 +0100

    Add help text to project author

--- softdesk/models/project.py
+++ softdesk/models/project.py
@@ -9,4 +9,5 @@ class Project(models.Model):
     title = models.CharField(max_length=180)
     description = models.TextField()
     project_type = models.CharField(choices=PROJECT_TYPE, max_length=100)
-    author_user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='project_author')
+    author_user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='project_author',
+                                    help_text='Author user is not used in permissions')
