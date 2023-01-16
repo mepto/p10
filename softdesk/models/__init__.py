885c458b51d224a1c59b134d28523cbe8712ce5e
mepto <natg.inf@gmail.com>
2023-01-16 18:54:59 +0100

    Fix missing contributor model in models init file

--- softdesk/models/__init__.py
+++ softdesk/models/__init__.py
@@ -1,5 +1,5 @@
 from softdesk.models.user import User
-# from softdesk.models.contributor import Contributor
+from softdesk.models.contributor import Contributor
 from softdesk.models.project import Project
 from softdesk.models.issue import Issue
 from softdesk.models.comment import Comment
