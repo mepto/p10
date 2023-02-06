67eecbdb62e6a27acd03d814286cce6f0f702e32
mepto <natg.inf@gmail.com>
2023-02-06 07:42:00 +0100

    Fix circular import issue

--- softdesk/models/contributor.py
+++ softdesk/models/contributor.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.db.models import UniqueConstraint
 
 from softdesk.constants import CONTRIBUTOR_ROLE
-from softdesk.models import Project, User
 
 
 class Contributor(models.Model):
     """Store contributors."""
 
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='contributors')
-    project = models.ForeignKey(to=Project, on_delete=models.CASCADE, related_name='contributors')
+    # User string notation instead of object reference to avoid circular import issue
+    user = models.ForeignKey(to='softdesk.User', on_delete=models.CASCADE, related_name='contributors')
+    project = models.ForeignKey(to='softdesk.Project', on_delete=models.CASCADE, related_name='contributors')
     role = models.CharField(choices=CONTRIBUTOR_ROLE, max_length=120)
 
     class Meta:
