26ffacd95ed3b7c806f4711e89cba16330b58b28
mepto <natg.inf@gmail.com>
2023-02-06 07:40:05 +0100

    Fix userviewset overriding create method

--- softdesk/views/user.py
+++ softdesk/views/user.py
@@ -1,5 +1,6 @@
 from django.utils.timezone import now
 from rest_framework import mixins, status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
@@ -10,16 +11,12 @@ class UserViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin,
                   mixins.ListModelMixin, GenericViewSet):
     """View for Users."""
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
 
-    def create(self, request, *args, **kwargs):
-        """Add creation data to user."""
-        serializer = self.get_serializer(data=request.data)
+    def perform_create(self, serializer):
+        """Add information to user creation."""
         serializer.initial_data['date_joined'] = now()
         serializer.initial_data['is_staff'] = False
         serializer.initial_data['is_superuser'] = False
         serializer.initial_data['is_active'] = True
-
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        serializer.save()
