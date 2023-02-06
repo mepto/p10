b6c0aac8f0bdd11672e11959b9f859e78b8dcb5d
mepto <natg.inf@gmail.com>
2023-02-06 07:43:41 +0100

    Remove raw mention from router urls

--- softdesk/urls.py
+++ softdesk/urls.py
@@ -11,14 +11,14 @@ from softdesk.views.project import ProjectViewSet
 from softdesk.views.user import UserViewSet
 
 router = DefaultRouter()
-router.register(r'projects', ProjectViewSet, basename='project')
-router.register(r'signup', UserViewSet, basename='signup')
+router.register('projects', ProjectViewSet, basename='project')
+router.register('signup', UserViewSet, basename='signup')
 
-projects_router = NestedSimpleRouter(router, r'projects', lookup='project')
-projects_router.register(r'issues', IssueViewSet, basename='issues')
-projects_router.register(r'users', ContributorViewSet, basename='users')
-issues_router = NestedSimpleRouter(projects_router, r'issues', lookup='issue')
-issues_router.register(r'comments', CommentViewSet, basename='comments')
+projects_router = NestedSimpleRouter(router, 'projects', lookup='project')
+projects_router.register('issues', IssueViewSet, basename='issues')
+projects_router.register('users', ContributorViewSet, basename='users')
+issues_router = NestedSimpleRouter(projects_router, 'issues', lookup='issue')
+issues_router.register('comments', CommentViewSet, basename='comments')
 
 
 urlpatterns = [
